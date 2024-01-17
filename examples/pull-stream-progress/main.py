a01b48814ae5918295efab348cd89e55d780ae27
Michael Yang <mxyng@pm.me>
2024-01-17 14:37:07 -0800

    add example for pulling with progress bar

---
+++ examples/pull-stream-progress/main.py
@@ -0,0 +1,23 @@
+# pip install tqdm
+
+from tqdm import tqdm
+from ollama import pull
+
+
+current_digest, bars = '', {}
+for progress in pull('mistral', stream=True):
+  digest = progress.get('digest', '')
+  if digest != current_digest and current_digest in bars:
+    bars[current_digest].close()
+
+  if not digest:
+    print(progress.get('status'))
+    continue
+
+  if digest not in bars and (total := progress.get('total')):
+    bars[digest] = tqdm(total=total, desc=f'pushing {digest[7:19]}', unit='B', unit_scale=True)
+
+  if completed := progress.get('completed'):
+    bars[digest].update(completed - bars[digest].n)
+
+  current_digest = digest
