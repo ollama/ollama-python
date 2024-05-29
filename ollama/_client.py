4c80c3a5af1413d158c966cb6c1418fc632774b3
Ben Plunkert <ben@plunkert.com>
2024-05-29 17:46:20 -0400

    Add URL path to client URL in in Client._parse_host()

--- ollama/_client.py
+++ ollama/_client.py
@@ -725,5 +725,6 @@ def _parse_host(host: Optional[str]) -> str:
   split = urllib.parse.urlsplit('://'.join([scheme, hostport]))
   host = split.hostname or '127.0.0.1'
   port = split.port or port
+  path = split.path or ''
 
-  return f'{scheme}://{host}:{port}'
+  return f'{scheme}://{host}:{port}{path}'
