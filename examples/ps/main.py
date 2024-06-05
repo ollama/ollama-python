9976af9a8f1f91aac7995eae70b3a9d8b9741c0f
Roy Han <royjhan02@gmail.com>
2024-06-05 13:38:42 -0700

    Developed Example

--- examples/ps/main.py
+++ examples/ps/main.py
@@ -1,4 +1,19 @@
-from ollama import ps
+from ollama import ps, pull, chat
+
+pull('mistral')
+chat('mistral', messages=[{'role': 'user', 'content': 'Pick a number'}])
 
 response = ps()
-print(response)
+
+name = response['models'][0]['name']
+size = response['models'][0]['size']
+size_vram = response['models'][0]['size_vram']
+
+if size == size_vram:
+    print(f'{name}: 100% GPU')
+elif not size_vram:
+    print(f'{name}: 100% CPU')
+else:
+    size_cpu = size - size_vram
+    cpu_percent = round(size_cpu / size * 100)
+    print(f'{name}: {cpu_percent}% CPU/{100 - cpu_percent}% GPU')
