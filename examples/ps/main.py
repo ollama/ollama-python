64794df5203e919405b283c99842ecf0f07c5732
Roy Han <royjhan02@gmail.com>
2024-06-05 13:42:40 -0700

    Lint

--- examples/ps/main.py
+++ examples/ps/main.py
@@ -10,10 +10,10 @@ size = response['models'][0]['size']
 size_vram = response['models'][0]['size_vram']
 
 if size == size_vram:
-    print(f'{name}: 100% GPU')
+  print(f'{name}: 100% GPU')
 elif not size_vram:
-    print(f'{name}: 100% CPU')
+  print(f'{name}: 100% CPU')
 else:
-    size_cpu = size - size_vram
-    cpu_percent = round(size_cpu / size * 100)
-    print(f'{name}: {cpu_percent}% CPU/{100 - cpu_percent}% GPU')
+  size_cpu = size - size_vram
+  cpu_percent = round(size_cpu / size * 100)
+  print(f'{name}: {cpu_percent}% CPU/{100 - cpu_percent}% GPU')
