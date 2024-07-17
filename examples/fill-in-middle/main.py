6c77302f7027d384350375f661239981fc18aaeb
royjhan <royjhan02@gmail.com>
2024-07-17 10:28:52 -0700

    update fill-in-the-middle example

--- examples/fill-in-middle/main.py
+++ examples/fill-in-middle/main.py
@@ -1,22 +1,9 @@
 from ollama import generate
 
-prefix = '''def remove_non_ascii(s: str) -> str:
-    """ '''
-
-suffix = """
-    return result
-"""
-
-
 response = generate(
-  model='codellama:7b-code',
-  prompt=f'<PRE> {prefix} <SUF>{suffix} <MID>',
-  options={
-    'num_predict': 128,
-    'temperature': 0,
-    'top_p': 0.9,
-    'stop': ['<EOT>'],
-  },
+  model='deepseek-coder-v2',
+  prompt='def add(',
+  suffix='return c',
 )
 
 print(response['response'])
