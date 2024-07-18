38ba3e6a4b3b9d1e96b2805f9339cdec8ba69ce6
royjhan <royjhan02@gmail.com>
2024-07-18 10:44:33 -0700

    keep example

--- examples/fill-in-middle/main.py
+++ examples/fill-in-middle/main.py
@@ -1,9 +1,16 @@
 from ollama import generate
 
 response = generate(
-  model='deepseek-coder-v2',
-  prompt='def add(',
-  suffix='return c',
+  model='codellama:7b-code',
+  prompt='def remove_non_ascii(s: str) -> str:',
+  suffix='return result',
+
+  options={
+    'num_predict': 128,
+    'temperature': 0,
+    'top_p': 0.9,
+    'stop': ['<EOT>'],
+  },
 )
 
 print(response['response'])
