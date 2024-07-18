3d93784581c9db8eecb170446fc04c49ad0635e0
royjhan <royjhan02@gmail.com>
2024-07-18 10:50:30 -0700

    variables

--- examples/fill-in-middle/main.py
+++ examples/fill-in-middle/main.py
@@ -1,9 +1,16 @@
 from ollama import generate
 
+prompt = '''def remove_non_ascii(s: str) -> str:
+    """ '''
+
+suffix = """
+    return result
+"""
+
 response = generate(
   model='codellama:7b-code',
-  prompt='def remove_non_ascii(s: str) -> str:',
-  suffix='return result',
+  prompt=prompt,
+  suffix=suffix,
   options={
     'num_predict': 128,
     'temperature': 0,
