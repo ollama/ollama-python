71c215b9af030e040c2f0cba82a511ed7fb483e9
Johannes Binder <j.binder.x@gmail.com>
2024-08-24 07:53:22 +0200

    Integrate feedback review to make it generally more stable and remove redundancies

--- ollama/_client.py
+++ ollama/_client.py
@@ -1,3 +1,4 @@
+import ipaddress
 import os
 import io
 import json
@@ -997,7 +998,27 @@ def _parse_host(host: Optional[str]) -> str:
   'http://example.com:56789/path'
   >>> _parse_host('[0001:002:003:0004::1]')
   'http://[0001:002:003:0004::1]:11434'
-  """
+  >>> _parse_host('[0001:002:003:0004::1]:56789')
+  'http://[0001:002:003:0004::1]:56789'
+  >>> _parse_host('http://[0001:002:003:0004::1]')
+  'http://[0001:002:003:0004::1]:80'
+  >>> _parse_host('https://[0001:002:003:0004::1]')
+  'https://[0001:002:003:0004::1]:443'
+  >>> _parse_host('https://[0001:002:003:0004::1]:56789')
+  'https://[0001:002:003:0004::1]:56789'
+  >>> _parse_host('[0001:002:003:0004::1]/')
+  'http://[0001:002:003:0004::1]:11434'
+  >>> _parse_host('[0001:002:003:0004::1]:56789/')
+  'http://[0001:002:003:0004::1]:56789'
+  >>> _parse_host('[0001:002:003:0004::1]/path')
+  'http://[0001:002:003:0004::1]:11434/path'
+  >>> _parse_host('[0001:002:003:0004::1]:56789/path')
+  'http://[0001:002:003:0004::1]:56789/path'
+  >>> _parse_host('https://[0001:002:003:0004::1]:56789/path')
+  'https://[0001:002:003:0004::1]:56789/path'
+  >>> _parse_host('[0001:002:003:0004::1]:56789/path/')
+  'http://[0001:002:003:0004::1]:56789/path'
+   """
 
   host, port = host or '', 11434
   scheme, _, hostport = host.partition('://')
@@ -1012,11 +1033,14 @@ def _parse_host(host: Optional[str]) -> str:
   host = split.hostname or '127.0.0.1'
   port = split.port or port
 
+  # Fix missing square brackets for IPv6 from urlsplit
+  try:
+    if isinstance(ipaddress.ip_address(host), ipaddress.IPv6Address):
+      host = f'[{host}]'
+  except ValueError:
+    ...
+
   if path := split.path.strip('/'):
     return f'{scheme}://{host}:{port}/{path}'
-
-  # Fix missing square brackets for IPv6 from urlsplit
-  if hostport.startswith('['):
-      host = f'[{host}]'
 
   return f'{scheme}://{host}:{port}'
