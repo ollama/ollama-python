25dc5064d104b6c4c75a6ed3809e48167e57f8a5
Samuel Colvin <s@muelcolvin.com>
2024-12-14 11:15:53 +0000

    format to 2 spaces for now :disappointed:

--- tests/test_live.py
+++ tests/test_live.py
@@ -8,46 +8,46 @@ from pydantic import BaseModel
 from ollama import AsyncClient
 
 pytestmark = [
-    pytest.mark.skipif(not os.getenv('OLLAMA_TEST_LIVE'), reason='live tests disabled'),
-    pytest.mark.asyncio,
+  pytest.mark.skipif(not os.getenv('OLLAMA_TEST_LIVE'), reason='live tests disabled'),
+  pytest.mark.asyncio,
 ]
 
 
 async def test_text():
-    client = AsyncClient()
-    response = await client.chat(
-        'qwen2:0.5b',
-        messages=[{'role': 'user', 'content': 'What is the capital of France?'}],
-        options={'temperature': 0},
-    )
-    print('Text response:', response)
-    assert 'paris' in response.message.content.lower()
+  client = AsyncClient()
+  response = await client.chat(
+    'qwen2:0.5b',
+    messages=[{'role': 'user', 'content': 'What is the capital of France?'}],
+    options={'temperature': 0},
+  )
+  print('Text response:', response)
+  assert 'paris' in response.message.content.lower()
 
 
 async def test_stream():
-    client = AsyncClient()
-    stream = await client.chat(
-        'qwen2:0.5b',
-        messages=[{'role': 'user', 'content': 'What is the capital of France?'}],
-        options={'temperature': 0},
-        stream=True,
-    )
-    text = ''.join([part['message']['content'] async for part in stream])
-    print('Text response:', text)
-    assert 'paris' in text.lower()
+  client = AsyncClient()
+  stream = await client.chat(
+    'qwen2:0.5b',
+    messages=[{'role': 'user', 'content': 'What is the capital of France?'}],
+    options={'temperature': 0},
+    stream=True,
+  )
+  text = ''.join([part['message']['content'] async for part in stream])
+  print('Text response:', text)
+  assert 'paris' in text.lower()
 
 
 class MyModel(BaseModel):
-    city: str
+  city: str
 
 
 async def test_structured():
-    client = AsyncClient()
-    response = await client.chat(
-        'qwen2:0.5b',
-        messages=[{'role': 'user', 'content': 'What is the capital of France?'}],
-        format=MyModel.model_json_schema(),
-        options={'temperature': 0},
-    )
-    my_model = MyModel.model_validate_json(response.message.content)
-    assert my_model.city.lower() == 'paris'
+  client = AsyncClient()
+  response = await client.chat(
+    'qwen2:0.5b',
+    messages=[{'role': 'user', 'content': 'What is the capital of France?'}],
+    format=MyModel.model_json_schema(),
+    options={'temperature': 0},
+  )
+  my_model = MyModel.model_validate_json(response.message.content)
+  assert my_model.city.lower() == 'paris'
