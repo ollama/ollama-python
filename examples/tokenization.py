588e338b2b601057697b78681c73747cebcfa043
ParthSareen <parth.sareen@ollama.com>
2024-12-16 11:34:03 -0800

    Add tests + cleanup

--- examples/tokenization.py
+++ examples/tokenization.py
@@ -1,10 +1,10 @@
 import ollama
 
 # Get tokens from a model
-response = ollama.tokenize(model='llama3.2', text='Hello world!')
+response = ollama.tokenize(model='llama3.2', text='Why the sky is blue?')
 tokens = response.tokens
-print('tokens from model', tokens)
+print('Tokens from model', tokens)
 
 # Convert tokens back to text
 response = ollama.detokenize(model='llama3.2', tokens=tokens)
-print('text from tokens', response.text)  # Prints: Hello world!
+print('Text from tokens', response.text)  # Prints: Why the sky is blue?
