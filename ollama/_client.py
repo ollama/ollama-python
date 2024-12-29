eb551ceeee33e1b36eb0fc7651ed6c54fdb45fd1
ParthSareen <parth.sareen@ollama.com>
2024-12-29 15:14:13 -0800

    Throw connection error instead

--- ollama/_client.py
+++ ollama/_client.py
@@ -118,7 +118,7 @@ class Client(BaseClient):
     except httpx.HTTPStatusError as e:
       raise ResponseError(e.response.text, e.response.status_code) from None
     except httpx.ConnectError:
-      raise ResponseError('Failed to connect to Ollama. Please check that Ollama is downloaded, running and accessible. https://ollama.com/download', 503) from None
+      raise ConnectionError('Failed to connect to Ollama. Please check that Ollama is downloaded, running and accessible. https://ollama.com/download') from None
 
   @overload
   def _request(
@@ -622,7 +622,7 @@ class AsyncClient(BaseClient):
     except httpx.HTTPStatusError as e:
       raise ResponseError(e.response.text, e.response.status_code) from None
     except httpx.ConnectError:
-      raise ResponseError('Failed to connect to Ollama. Please check that Ollama is downloaded, running and accessible. https://ollama.com/download', 503) from None
+      raise ConnectionError('Failed to connect to Ollama. Please check that Ollama is downloaded, running and accessible. https://ollama.com/download') from None
 
   @overload
   async def _request(
