7a6ab044c610ba7475180d3d1dd451e57661757a
Jeffrey Morgan <jmorganca@gmail.com>
2025-01-15 22:43:44 -0800

    Fix create examples (#421)

--- examples/create.py
+++ examples/create.py
@@ -1,5 +1,10 @@
 from ollama import Client
 
 client = Client()
-response = client.create(model='my-assistant', from_='llama3.2', stream=False)
+response = client.create(
+    model='my-assistant',
+    from_='llama3.2',
+    system="You are mario from Super Mario Bros.",
+    stream=False
+)
 print(response.status)
