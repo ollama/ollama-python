09c527237dba0e0b70e901a4a6934c8d9b9428f4
Aarni Koskela <akx@iki.fi>
2025-01-16 09:14:23 +0200

    Run ruff-format so CI doesn't complain

--- examples/create.py
+++ examples/create.py
@@ -2,9 +2,9 @@ from ollama import Client
 
 client = Client()
 response = client.create(
-    model='my-assistant',
-    from_='llama3.2',
-    system="You are mario from Super Mario Bros.",
-    stream=False
+  model='my-assistant',
+  from_='llama3.2',
+  system='You are mario from Super Mario Bros.',
+  stream=False,
 )
 print(response.status)
