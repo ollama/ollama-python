5ecfaab50ad281fc4c87ed602933235cac0058f0
Susan Dhungana <susandhungana20@gmail.com>
2026-04-30 10:16:23 +0545

    feat: expose __version__ and top-level version() function

--- ollama/__init__.py
+++ ollama/__init__.py
@@ -1,44 +1,49 @@
+from importlib.metadata import version as _pkg_version
 from ollama._client import AsyncClient, Client
 from ollama._types import (
-  ChatResponse,
-  EmbeddingsResponse,
-  EmbedResponse,
-  GenerateResponse,
-  Image,
-  ListResponse,
-  Message,
-  Options,
-  ProcessResponse,
-  ProgressResponse,
-  RequestError,
-  ResponseError,
-  ShowResponse,
-  StatusResponse,
-  Tool,
-  WebFetchResponse,
-  WebSearchResponse,
+    ChatResponse,
+    EmbeddingsResponse,
+    EmbedResponse,
+    GenerateResponse,
+    Image,
+    ListResponse,
+    Message,
+    Options,
+    ProcessResponse,
+    ProgressResponse,
+    RequestError,
+    ResponseError,
+    ShowResponse,
+    StatusResponse,
+    Tool,
+    WebFetchResponse,
+    WebSearchResponse,
 )
 
+__version__ = _pkg_version('ollama')
+
 __all__ = [
-  'AsyncClient',
-  'ChatResponse',
-  'Client',
-  'EmbedResponse',
-  'EmbeddingsResponse',
-  'GenerateResponse',
-  'Image',
-  'ListResponse',
-  'Message',
-  'Options',
-  'ProcessResponse',
-  'ProgressResponse',
-  'RequestError',
-  'ResponseError',
-  'ShowResponse',
-  'StatusResponse',
-  'Tool',
-  'WebFetchResponse',
-  'WebSearchResponse',
+    'AsyncClient',
+    'ChatResponse',
+    'Client',
+    'EmbedResponse',
+    'EmbeddingsResponse',
+    'GenerateResponse',
+    'Image',
+    'ListResponse',
+    'Message',
+    'Options',
+    'ProcessResponse',
+    'ProgressResponse',
+    'RequestError',
+    'ResponseError',
+    'ShowResponse',
+    'StatusResponse',
+    'Tool',
+    'WebFetchResponse',
+    'WebSearchResponse',
+    '__version__',
+    'version',
 ]
 
 _client = Client()
@@ -52,8 +57,7 @@ push = _client.push
 create = _client.create
 delete = _client.delete
 list = _client.list
-copy = _client.copy
 show = _client.show
+copy = _client.copy
 ps = _client.ps
-web_search = _client.web_search
-web_fetch = _client.web_fetch
+version = _client.version   # Expose the version method for users to check the Ollama version
